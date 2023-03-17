e6f8489e6a984dea97349d8056c8b8b9a7e2fff7
anitkumarsingh <anikumarsingh99@gmail.com>
2023-03-17 21:53:16 +0530

    meaningful content rendering

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,1 +1,20 @@
-console.log('HI THere');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const mount = (el) =>{
+
+  ReactDOM.render(<h1>Hi,There</h1>,el);
+
+}
+
+// delelopment or production
+if(process.env.NODE_ENV ==='development'){
+  const findEl = document.querySelector('#marketing-dev-root');
+  if(findEl){
+    mount(findEl);
+  }
+}
+
+//
+
+module.export = mount;
