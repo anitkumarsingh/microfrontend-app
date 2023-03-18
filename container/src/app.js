aef9b77310a980a03ac0861f872d1d9adf1c348c
anitkumarsingh <anikumarsingh99@gmail.com>
2023-03-18 17:50:31 +0530

    avoiding classname collision in production

--- container/src/app.js
+++ container/src/app.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import MarketingApp from './conponents/MarketingApp';
 import Header from './conponents/Header';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+
+const generateRandomClassname = createGenerateClassName({
+	productionPrefix: 'co'
+});
 
 const App = () => {
 	return (
-		<BrowserRouter>
-		  <Header/>
-			<MarketingApp />
-		</BrowserRouter>
+		<StylesProvider generateClassName={generateRandomClassname}>
+			<BrowserRouter>
+				<Header />
+				<MarketingApp />
+			</BrowserRouter>
+		</StylesProvider>
 	);
 };
 
--- marketing/src/app.js
+++ marketing/src/app.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core';
+import { StylesProvider,createGenerateClassName } from '@material-ui/core';
 import LandingPage from './components/Landing';
 import Pricing from './components/Pricing';
+
+const generateRandomClassname= createGenerateClassName({
+	productionPrefix:'ma'
+});
 
 const App = () => {
 	return (
 		<>
-			<StylesProvider>
+			<StylesProvider generateClassName={generateRandomClassname}>
 				<BrowserRouter>
 					<Switch>
 						<Route exact path='/' component={LandingPage} />
