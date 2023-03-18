7d002402f547b84daa17fb98f56ca118b021f1ce
anitkumarsingh <anikumarsingh99@gmail.com>
2023-03-18 19:41:36 +0530

    syncing communication

--- container/src/conponents/MarketingApp.js
+++ container/src/conponents/MarketingApp.js
@@ -1,15 +1,20 @@
-import React,{useRef,useEffect} from 'react';
-import {mount} from 'marketing/MarketingApp';
+import React, { useRef, useEffect } from 'react';
+import { mount } from 'marketing/MarketingApp';
+import { useHistory } from 'react-router-dom';
 
-const MarketingApp = () =>{
-  const ref = useRef(null);
-  useEffect(()=>{
-    mount(ref.current,{
-      onNavigate:()=>{
-        console.log('The container noticed navigation in marketing')
-      }
-    })
-  },[])
-  return(<div ref={ref}></div>)
-}
+const MarketingApp = () => {
+	const ref = useRef(null);
+	const history = useHistory();
+	useEffect(() => {
+		mount(ref.current, {
+			onNavigate: ({ pathname: nextPathname }) => {
+				const { pathname } = history.location;
+				if (pathname !== nextPathname) {
+					history.push(nextPathname);
+				}
+			}
+		});
+	}, []);
+	return <div ref={ref}></div>;
+};
 export default MarketingApp;
