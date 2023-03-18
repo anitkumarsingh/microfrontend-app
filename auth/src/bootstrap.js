23116f4c6ea36af42e502554e2505d29e8ed39a8
anitkumarsingh <anikumarsingh99@gmail.com>
2023-03-18 23:02:39 +0530

    initial state

--- auth/src/bootstrap.js
+++ auth/src/bootstrap.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import { createMemoryHistory,createBrowserHistory } from 'history';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
-const mount = (el, { onNavigate,defaultHistory }) => {
-	const history = defaultHistory || createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+	const history =
+		defaultHistory ||
+		createMemoryHistory({
+			initialEntries: [initialPath]
+		});
 	if (onNavigate) {
 		history.listen(onNavigate);
 	}
@@ -25,7 +29,7 @@ const mount = (el, { onNavigate,defaultHistory }) => {
 if (process.env.NODE_ENV === 'development') {
 	const findEl = document.querySelector('#auth-dev-root');
 	if (findEl) {
-		mount(findEl, {defaultHistory:createBrowserHistory()});
+		mount(findEl, { defaultHistory: createBrowserHistory() });
 	}
 }
 
