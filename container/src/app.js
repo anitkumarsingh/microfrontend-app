f1cb66c29ff06bf0e92738fb99647bbf2a7728ba
anitkumarsingh <anikumarsingh99@gmail.com>
2023-03-19 22:32:06 +0530

    protecting route

--- container/src/app.js
+++ container/src/app.js
@@ -1,8 +1,9 @@
-import React, { lazy, Suspense, useState } from 'react';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
 import Header from './conponents/Header';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Redirect, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 import ProgressBar from './conponents/Progress';
+import { createBrowserHistory } from 'history';
 
 const LazyMarketingApp = lazy(() => import('./conponents/MarketingApp'));
 const LazyAuthApp = lazy(() => import('./conponents/AuthApp'));
@@ -12,22 +13,36 @@ const generateRandomClassname = createGenerateClassName({
 	productionPrefix: 'co'
 });
 
+const history = createBrowserHistory();
+
 const App = () => {
 	const [isSignIn, setIsSignIn] = useState(false);
+
+	useEffect(() => {
+		if (isSignIn) {
+			history.push('/dashboard');
+		}
+	}, [isSignIn]);
+
 	return (
 		<StylesProvider generateClassName={generateRandomClassname}>
-			<BrowserRouter>
+			<Router history={history}>
 				<Header isSignIn={isSignIn} onSignOut={() => setIsSignIn(false)} />
 				<Switch>
 					<Suspense fallback={<ProgressBar />}>
 						<Route path='/auth'>
 							<LazyAuthApp onSignIn={() => setIsSignIn(true)} />
 						</Route>
-						<Route path='/dashboard' component={LazyDashboardApp} />
+
+						<Route path='/dashboard'>
+							{!isSignIn && <Redirect to='/' />}
+							<LazyDashboardApp />
+						</Route>
+
 						<Route path='/' component={LazyMarketingApp} />
 					</Suspense>
 				</Switch>
-			</BrowserRouter>
+			</Router>
 		</StylesProvider>
 	);
 };
