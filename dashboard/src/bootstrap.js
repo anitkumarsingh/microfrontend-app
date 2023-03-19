fdd81058c8b751d3f3a56e5fddc97a85c3bd4fb0
anitkumarsingh <anikumarsingh99@gmail.com>
2023-03-19 21:37:11 +0530

    dashboard

---
+++ dashboard/src/bootstrap.js
@@ -0,0 +1,19 @@
+import Dashboard from './components/Dashboard.vue';
+import {createApp} from 'vue';
+
+const mount = (el) => {
+	const app = createApp(Dashboard);
+	app.mount(el);
+};
+
+// delelopment or production
+if (process.env.NODE_ENV === 'development') {
+	const findEl = document.querySelector('#dashboard-dev-root');
+	if (findEl) {
+		mount(findEl);
+	}
+}
+
+//
+
+export { mount };
