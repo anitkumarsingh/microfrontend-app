c791f254f6c550e08d58e7b105060afa62341dd2
anitkumarsingh <anikumarsingh99@gmail.com>
2023-03-19 16:13:18 +0530

    minor change

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -3,8 +3,6 @@ const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 
-const domain = process.env.PRODUCTION_DOMAIN;
-
 const prodConfig = {
 	mode: 'production',
 	output: {
