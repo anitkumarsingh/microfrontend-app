9aa755dbbe800e116e1bf38e6222f24a5cc4e98d
anitkumarsingh <anikumarsingh99@gmail.com>
2023-03-19 15:32:00 +0530

    testing deployment

--- container/src/app.js
+++ container/src/app.js
@@ -13,7 +13,6 @@ const generateRandomClassname = createGenerateClassName({
 
 const App = () => {
 	const [isSignIn, setIsSignIn] = useState(false);
-	console.log('asdasas');
 	return (
 		<StylesProvider generateClassName={generateRandomClassname}>
 			<BrowserRouter>
