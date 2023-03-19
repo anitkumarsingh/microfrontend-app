0a8ede2a883b21cd5eb943d03f56c9360c8258dc
anitkumarsingh <anikumarsingh99@gmail.com>
2023-03-19 11:27:46 +0530

    lazy loading

--- container/src/app.js
+++ container/src/app.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import MarketingApp from './conponents/MarketingApp';
-import AuthApp from './conponents/AuthApp';
+import React,{lazy,Suspense} from 'react';
 import Header from './conponents/Header';
 import { BrowserRouter,Route,Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+
+const lazyMarketingApp = lazy(()=>import('./conponents/MarketingApp'));
+const lazyAuthApp = lazy(()=>import('./conponents/AuthApp'));
 
 const generateRandomClassname = createGenerateClassName({
 	productionPrefix: 'co'
@@ -15,10 +16,11 @@ const App = () => {
 			<BrowserRouter>
 				<Header />
 				<Switch>
-					<Route path="/auth" component={AuthApp}/>
-					<Route path="/" component={MarketingApp}/>
+					<Suspense fallback={<h1>Loading...</h1>}>
+					<Route path="/auth" component={lazyAuthApp}/>
+					<Route path="/" component={lazyMarketingApp}/>
+					</Suspense>
 				</Switch>
-				<MarketingApp />
 			</BrowserRouter>
 		</StylesProvider>
 	);
